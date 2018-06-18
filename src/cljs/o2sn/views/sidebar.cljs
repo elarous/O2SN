c5fa7e9e3f962f899157f3fe83de88ddd370b9f1
oussamaelarbaoui <elarbaouioussama@gmail.com>
2018-06-19 00:27:30 +0100

    make menu items in  the sidebar menu active on click, and disabled if already selected

--- src/cljs/o2sn/views/sidebar.cljs
+++ src/cljs/o2sn/views/sidebar.cljs
@@ -4,6 +4,16 @@
             [secretary.core :as secretary]
             [o2sn.ui :as ui]))
 
+
+(defn menu-item [{:keys [panel label icon]}]
+  (let [active? @(rf/subscribe [:active-panel? panel])]
+    [ui/menu-item {:name (name panel)
+                   :link true
+                   :active active?
+                   :disabled active?
+                   :on-click #(secretary/dispatch! (str "/" (name panel)))}
+     [ui/icon {:name icon}] label]))
+
 (defn side-bar [panel]
   [ui/sidebar-pushable {:as (ui/component "Segment")}
    [ui/sidebar {:as (ui/component "Menu")
@@ -14,35 +24,11 @@
                 :vertical true
                 :color "teal"
                 :inverted true}
-    [ui/menu-item {:name "home"
-                   :link true
-                   :active @(rf/subscribe [:active-panel? :home])
-                   :on-click #(secretary/dispatch! "/home")}
-     [ui/icon {:name "home"}]
-     "Home"]
-    [ui/menu-item {:name "channels"
-                   :link true
-                   :active @(rf/subscribe [:active-panel? :channels])}
-     [ui/icon {:name "podcast"}]
-     "Channels"]
-    [ui/menu-item {:name "messages"
-                   :link true
-                   :active @(rf/subscribe [:active-panel? :messages])
-                   :on-click #(secretary/dispatch! "/messages")}
-     [ui/icon {:name "envelope"}]
-     "Messages"]
-
-    [ui/menu-item {:name "friends"
-                   :active @(rf/subscribe [:active-panel? :friends])
-                   :link true}
-     [ui/icon {:name "users"}]
-     "Friends"]
-
-    [ui/menu-item {:name "settings"
-                   :active @(rf/subscribe [:active-panel? :settings])
-                   :link true}
-     [ui/icon {:name "settings"}]
-     "Settings"]]
+    [menu-item {:panel  :home :label "Home" :icon  "home"}]
+    [menu-item {:panel :channels :label "Channels" :icon "podcast"}]
+    [menu-item {:panel :messages :label "Messages" :icon "envelope"}]
+    [menu-item {:panel :friends :label "Friends" :icon "users"}]
+    [menu-item {:panel :settings :label "Settings" :icon "settings"}]]
    [ui/sidebar-pusher
     [ui/segment {:basic true}
      panel]]])
